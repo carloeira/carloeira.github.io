37862476fd186c3e5868762e8fd50a7d9f0bc97d
Carlos Vieira <112698218+carloeira@users.noreply.github.com>
2024-07-04 01:42:51 -0300

    first commit in new application

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <code className='code'>&gt;_</code>
+        </p>
+        <p>
+          Site em construção
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
